e62fa1d48de904666fb78936f40eb045d47475d4
Katherine <katherineleefoley@gmail.com>
2022-11-01 19:52:36 -0500

    im not sure how far back this is adding

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # About-Kati
+
 html, css, js (toUpperCase() toLowerCase()) reviewing alert and prompt, git ACP
--- js/app.js
+++ js/app.js
@@ -1,2 +1,41 @@
 'use strict';
 console.log('app js is connected');
+
+//prompt our users for some input
+
+let userName = prompt('What is your name?');
+
+console.log('user name is: ', userName);
+
+
+//convert to lowerCase
+
+//userName.toLowerCase(); or the userName.toUpperCase();
+let lowerCaseName = userName.toLowerCase();
+
+console.log('Namer Lower Case: ', lowerCaseName)
+
+
+//validate the input using some coditional logic
+//if(meet this condition then do the else){
+
+//} else {
+
+//}
+if(lowerCaseName === 'bob' || lowerCaseName === 'larry'){
+    console.log('we are comparing lowercase:', lowerCaseName);
+    console.log('we are using his user name he provided', userName);
+    alert('Welcome back to the site: ' + userName);
+} else {
+    alert('Welcome new user to my page!')
+}
+
+
+
+
+//look at some data types: boolean, string, number, 
+
+
+
+
+//introduction to arrays
